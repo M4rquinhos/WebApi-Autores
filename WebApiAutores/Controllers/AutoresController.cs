c6f8d363275d1e479cd913c36bf8dc1ee038ac1c
Marco Antonio Carrillo Esparza <marco.37099p@gmail.com>
2024-03-29 19:00:12 -0600

    Agregar archivos de proyecto.

---
+++ WebApiAutores/Controllers/AutoresController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiAutores.Entidades;
+
+namespace WebApiAutores.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutoresController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Autor>> Get()
+        {
+            return new List<Autor>() { 
+                new Autor { Id = 1, Nombre = "Marco" },  
+                new Autor { Id = 2, Nombre = "David"}
+            };
+        }
+    }
+}
